125b46b0aed267ed88d91f41c5db3cbf8da4ac35
usagi <usagi@WonderRabbitProject.net>
2012-06-21 08:35:02 +0900

    first

---
+++ obj/local/armeabi/objs-debug/test1/test1.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs-debug/test1/test1.o: jni/test1.cpp \
+ /opt/android-ndk/platforms/android-14/arch-arm/usr/include/string.h \
+ /opt/android-ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /opt/android-ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /opt/android-ndk/platforms/android-14/arch-arm/usr/include/jni.h \
+ /opt/android-ndk/sources/cxx-stl/gnu-libstdc++/include/string
+
+/opt/android-ndk/platforms/android-14/arch-arm/usr/include/string.h:
+
+/opt/android-ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/opt/android-ndk/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/opt/android-ndk/sources/cxx-stl/gnu-libstdc++/include/string:
